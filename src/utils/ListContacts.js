89642b9be1d67407be02d9b73951c4ddf8475302
Alex Cho <alexsjcho@gmail.com>
2019-03-12 18:38:23 -0700

    refactored ListCnotacts to functional component

--- src/utils/ListContacts.js
+++ src/utils/ListContacts.js
@@ -1,27 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ListContacts extends Component {
-  render() {
-    return (
-      <ol className="contact-list">
-        {this.props.contacts.map(contact => (
-          <li key={contact.id} className="conact-list-item">
-            <div
-              className="contact-avatar"
-              style={{
-                backgroundImage: `url(${contact.avatarURL})`
-              }}
-            />
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.handle}</p>
-            </div>
-            <button className="contact-remove">Remove</button>
-          </li>
-        ))}
-      </ol>
-    );
-  }
-}
+const ListContacts = props => {
+  return (
+    <ol className="contact-list">
+      {this.contacts.map(contact => (
+        <li key={contact.id} className="conact-list-item">
+          <div
+            className="contact-avatar"
+            style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}
+          />
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.handle}</p>
+          </div>
+          <button className="contact-remove">Remove</button>
+        </li>
+      ))}
+    </ol>
+  );
+};
 
 export default ListContacts;
